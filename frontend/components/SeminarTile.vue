ba9f773e4f5734e7e338b04e5ca36e6749e0eb04
Schaeferjo <mail@joschaefer.com>
2020-07-08 17:11:46 +0200

    New component => seminar

---
+++ frontend/components/SeminarTile.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="seminar-tile">
+    <h1>{{ seminar.title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    seminar: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,86 +1,40 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <div class="text-center"></div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+      <seminar-tile
+        v-for="seminar in seminars"
+        :key="seminar.id"
+        :seminar="seminar"
+      >
+      </seminar-tile>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import SeminarTile from '../components/SeminarTile.vue'
+
 export default {
-  components: {},
+  components: {
+    SeminarTile,
+  },
+  data() {
+    return {
+      seminars: [
+        {
+          id: 1,
+          title: 'Testseminar Title',
+          created_at: '2020-07-08T14:21:03.862Z',
+          updated_at: '2020-07-08T14:23:50.817Z',
+        },
+        {
+          id: 2,
+          title: 'Zweiter Testseminar Title',
+          created_at: '2020-07-08T14:24:11.487Z',
+          updated_at: '2020-07-08T14:24:11.487Z',
+        },
+      ],
+    }
+  },
 }
 </script>
