0d03bbfb2d47fc7d69bb155eb3d0169e0fb2b41a
roschaefer <git@roschaefer.de>
2020-07-09 20:29:13 +0200

    feat: send emails with nodemailer

--- backend/api/buchung/models/buchung.js
+++ backend/api/buchung/models/buchung.js
@@ -5,4 +5,19 @@
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+
+   async afterCreate(result, data) {
+     const { email_adresse, name } = data
+     strapi.log.debug('Buchung created!', result, data)
+     await strapi.plugins['email'].services.email.send({
+       to: email_adresse,
+       from: 'werkhof-ichen@example.org',
+       subject: 'Use strapi email provider successfully',
+       text: 'Hello world!',
+       html: 'Hello world!',
+     });
+    }
+  }
+};
