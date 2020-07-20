f3f0f4f55b64efc0398071d32ad43e030cae25f7
Schaeferjo <mail@joschaefer.com>
2020-07-20 20:16:34 +0200

    Add some content to the ueber_mich page

--- frontend/pages/ueber_mich.vue
+++ frontend/pages/ueber_mich.vue
@@ -1,5 +1,34 @@
 an<template>
   <v-layout>
-    <h1>über mich</h1>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-img
+            src="/header_images/header_leather.jpg"
+            alt="werkhof ichen: Eine Auswahl von Leder"
+            class="mb-5"
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="col-4" justify="end"></v-col>
+        <v-col class="col-8">
+          <h1>ichen</h1>
+          <p>
+            ... kommt von "Kleines I" i-chen und ist seit Urzeiten mein Spitzname.
+            <br />Offiziell: Irmina Maria Schmitz.
+          </p>
+
+          <p>Spaß an der Näherei hatte ich bereits mit 10 Jahren. Deswegen biete ich heutzutage auch gerne Kindernähkurse an.</p>
+
+          <p>1987 erhielt ich mein Diplom der Bekleidungstechnik in Mönchengladbach.</p>
+
+          <p>
+            Bereits seit 1988 biete ich Nähkurse an.
+            Seit 1991 betreibe ich den "werkhof ichen" und bin selbständig tätig.
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-layout>
 </template>
