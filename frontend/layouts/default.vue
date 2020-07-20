db11a606dfccd3b6d4aa011d40e6499504e2bba5
Schaeferjo <mail@joschaefer.com>
2020-07-20 15:05:17 +0200

    Mobile menu: click on hamburger icon revels the menu

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,23 +1,39 @@
 <template>
-  <v-app dark>
-    <v-app-bar height="100px" flat color="rgba(255,255,255,0)" app absolute>
-      <v-toolbar-title v-text="title" />
-
+  <v-app>
+    <v-navigation-drawer v-model="drawer" fixed app>
       <v-btn text nuxt link to="/">Start</v-btn>
+      <v-menu offset-y open-on-hover>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text v-bind="attrs" v-on="on">Veranstaltungen</v-btn>
+        </template>
+        <v-list>
+          <nuxt-link
+            to="/veranstaltungen/kurse_fuer_kinder_und_jugendliche"
+          >Kurse für Kinder und Jugendliche</nuxt-link>
+          <nuxt-link to="/veranstaltungen/kurse_fuer_erwachsene">Kurse für Erwachsene</nuxt-link>
+          <nuxt-link to="/veranstaltungen/geburtstagsevents">Geburtstagsevents</nuxt-link>
+          <nuxt-link to="/veranstaltungen/anmelden">Anmelden</nuxt-link>
+        </v-list>
+      </v-menu>
       <v-menu offset-y open-on-hover>
         <template v-slot:activator="{ on, attrs }">
           <v-btn text v-bind="attrs" v-on="on">Anfertigungen</v-btn>
         </template>
         <v-list>
           <nuxt-link to="/anfertigungen/leder">Leder</nuxt-link>
-          <nuxt-link to="/anfertigungen/kindergeburtstag">Kindergeburtstag</nuxt-link>
+          <nuxt-link to="/anfertigungen/weitere_textilien">Weitere Textilien</nuxt-link>
         </v-list>
       </v-menu>
       <v-btn text nuxt link to="/kurse">Kurse</v-btn>
 
-      <v-btn text nuxt link to="/anmelden">Anmelden</v-btn>
-
       <v-btn text nuxt link to="/kontakt">Kontakt</v-btn>
+      <v-btn text nuxt link to="/ueber_mich">Über mich</v-btn>
+    </v-navigation-drawer>
+
+    <v-app-bar height="100px" flat color="rgba(255,255,255,0)" app absolute>
+      <v-toolbar-title v-text="title" />
+
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
     </v-app-bar>
 
     <v-main>
@@ -44,7 +60,9 @@
         </v-row>
 
         <v-row justify="center">
-          <v-col cols="2">LOGO</v-col>
+          <v-col cols="2">
+            <img src="/logo_ichen.svg" alt="Logo Werkhof ichen" class="mb-5" />
+          </v-col>
           <v-col cols="2">
             <strong>werkhof ichen</strong>
             <br />02246 . 31 97
@@ -74,6 +92,7 @@ export default {
     return {
       fixed: false,
       title: 'werkhof ichen',
+      drawer: false,
     }
   },
 }
@@ -84,56 +103,55 @@ body {
   background-repeat: no-repeat;
   background-position: 50% 50%;
   background-size: contain;
-  background-image: url(../static/bg.jpg) !important;
-}
-
-#footer_gallery {
-  width: 100%;
-  height: 10vw;
-  display: flex;
-  div {
-    flex-grow: 10;
-    height: 10;
-    display: inline-block;
-    margin: 0;
-    padding: 0;
-    background-size: cover;
-    background-position: 50% 50%;
-    background-repeat: no-repeat;
-    &.house {
-      background-image: url(../static/footer_images/house.jpg);
-    }
-    &.shield {
-      background-image: url(../static/footer_images/shield.jpg);
-    }
-    &.collection1 {
-      background-image: url(../static/footer_images/collection1.jpg);
-    }
-    &.garden {
-      background-image: url(../static/footer_images/garden.jpg);
-    }
-    &.sew {
-      background-image: url(../static/footer_images/sew.jpg);
-    }
-    &.collection2 {
-      background-image: url(../static/footer_images/collection2.jpg);
-    }
-    &.kater_schroeder {
-      background-image: url(../static/footer_images/kater_schroeder.jpg);
-    }
-    &.flower {
-      background-image: url(../static/footer_images/flower.jpg);
-    }
-    &.texture {
-      background-image: url(../static/footer_images/texture.jpg);
-    }
-    &.lamp {
-      background-image: url(../static/footer_images/lamp.jpg);
-    }
-  }
+  background-image: url('../static/bg.jpg') !important;
 }
 
 footer {
   color: #fff !important;
+  #footer_gallery {
+    width: 100%;
+    height: 10vw;
+    display: flex;
+    div {
+      flex-grow: 10;
+      height: 10;
+      display: inline-block;
+      margin: 0;
+      padding: 0;
+      background-size: cover;
+      background-position: 50% 50%;
+      background-repeat: no-repeat;
+      &.house {
+        background-image: url(../static/footer_images/house.jpg);
+      }
+      &.shield {
+        background-image: url(../static/footer_images/shield.jpg);
+      }
+      &.collection1 {
+        background-image: url(../static/footer_images/collection1.jpg);
+      }
+      &.garden {
+        background-image: url(../static/footer_images/garden.jpg);
+      }
+      &.sew {
+        background-image: url(../static/footer_images/sew.jpg);
+      }
+      &.collection2 {
+        background-image: url(../static/footer_images/collection2.jpg);
+      }
+      &.kater_schroeder {
+        background-image: url(../static/footer_images/kater_schroeder.jpg);
+      }
+      &.flower {
+        background-image: url(../static/footer_images/flower.jpg);
+      }
+      &.texture {
+        background-image: url(../static/footer_images/texture.jpg);
+      }
+      &.lamp {
+        background-image: url(../static/footer_images/lamp.jpg);
+      }
+    }
+  }
 }
 </style>
