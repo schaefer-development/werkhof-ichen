4777dd59b06b9a10752ca7a06b76d407b81d820c
Schaeferjo <mail@joschaefer.com>
2020-07-30 15:27:22 +0200

    Separated footer in a single component and styled the content

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -45,55 +45,20 @@
       </v-layout>
     </v-main>
 
-    <v-footer color="rgba(72,65,61,0.9)" padless>
-      <v-container fluid pt-0 pb-3>
-        <v-row>
-          <div id="footer_gallery">
-            <div class="house"></div>
-            <div class="etui"></div>
-            <div class="collection1"></div>
-            <div class="hearts"></div>
-            <div class="sew"></div>
-            <div class="collection2"></div>
-            <div class="kater_schroeder"></div>
-            <div class="flower"></div>
-            <div class="texture"></div>
-            <div class="lamp"></div>
-          </div>
-        </v-row>
-
-        <v-row justify="center" class="pt-6">
-          <v-col xs="12" sm="6" md="2" align="center">
-            <v-img src="/logo_ichen_outline.svg" alt="Logo Werkhof ichen" max-width="80px" />
-          </v-col>
-          <v-col xs="12" sm="6" md="2">
-            <strong>werkhof ichen</strong>
-            <br />02246 . 31 97
-            <br />
-            <a href="mailto:info@werkhof-ichen.de">info@werkhof-ichen.de</a>
-          </v-col>
-          <v-col xs="12" sm="6" md="2">
-            Datenschutz
-            <br />Impressum
-            <br />
-          </v-col>
-          <v-col xs="12" sm="6" md="2">
-            AGB
-            <br />
-            Copyright {{ new Date().getFullYear() }}
-          </v-col>
-        </v-row>
-      </v-container>
+    <v-footer padless>
+      <footer-component></footer-component>
     </v-footer>
   </v-app>
 </template>
 
 <script>
 import NavigationItems from '../components/NavigationItems.vue'
+import FooterComponent from '../components/FooterComponent.vue'
 
 export default {
     components: {
     NavigationItems,
+    FooterComponent,
   },
   data() {
     return {
@@ -197,54 +162,5 @@ export default {
 .custom_max_width {
   max-width: 1185px !important;
   margin: auto;
-}
-
-footer {
-  color: #fff !important;
-  #footer_gallery {
-    width: 100%;
-    height: 10vw;
-    display: flex;
-    div {
-      flex-grow: 10;
-      height: 10;
-      display: inline-block;
-      margin: 0;
-      padding: 0;
-      background-size: cover;
-      background-position: 50% 100%;
-      background-repeat: no-repeat;
-      &.house {
-        background-image: url(../static/footer_images/house.jpg);
-      }
-      &.etui {
-        background-image: url(../static/footer_images/etui.jpg);
-      }
-      &.collection1 {
-        background-image: url(../static/footer_images/collection1.jpg);
-      }
-      &.hearts {
-        background-image: url(../static/footer_images/hearts.jpg);
-      }
-      &.sew {
-        background-image: url(../static/footer_images/sew.jpg);
-      }
-      &.collection2 {
-        background-image: url(../static/footer_images/collection2.jpg);
-      }
-      &.kater_schroeder {
-        background-image: url(../static/footer_images/kater_schroeder.jpg);
-      }
-      &.flower {
-        background-image: url(../static/footer_images/flower.jpg);
-      }
-      &.texture {
-        background-image: url(../static/footer_images/texture.jpg);
-      }
-      &.lamp {
-        background-image: url(../static/footer_images/lamp.jpg);
-      }
-    }
-  }
 }
 </style>
