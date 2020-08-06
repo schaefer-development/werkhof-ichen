5b43d55d9f4cd1a02db0358ced67891b67b7d6df
Schaeferjo <mail@joschaefer.com>
2020-08-06 17:00:56 +0200

    Add modul date-fns and modified e-mail text, when booked

--- backend/api/buchung/models/buchung.js
+++ backend/api/buchung/models/buchung.js
@@ -5,68 +5,69 @@
  * to customize this model
  */
 
+const { format } = require('date-fns')
+const { addDays } = require('date-fns')
+
 module.exports = {
   lifecycles: {
 
    async afterCreate(result, data) {
+     const veranstaltungs_datum = Date.parse (result.veranstaltung.Datum)
+     const date_ahead = addDays(veranstaltungs_datum, 14)
+     const date_payment = new Date(Math.min ( veranstaltungs_datum, date_ahead ))
      const { email_adresse, name } = data
      strapi.log.debug('Buchung created!', result, data)
      await strapi.plugins['email'].services.email.send({
        to: email_adresse,
        from: 'werkhof-ichen@example.org',
-       subject: 'Ihre Buchung auf werkhof-ichen.de',
+       subject: 'Ihre Anmeldung auf werkhof-ichen.de',
        text: `
-       Hallo  ${ result.name }, 
-       vielen Dank für Ihre Buchung. 
-       Kursname
-       Datum
-       Bitte überweisen Sie den Betrag in Höhe von PREIS innerhalb von XX Tagen auf folgendes Konto:
-       
-       IBAN: DE24370502990163007123 
-       BIC: COKSDE33<br />
-       Bank: Kreissparkasse Köln
-       
-       Der Teilnehmerplatz ist erst mit Zahlungseingang verbindlich reserviert.
-       Viele Grüße und bis bald!
-       ichen    
+Hallo  ${ result.name }, 
 
-       Rechtliche Hinweise:
-       Der Teilnehmerplatz wird für mich nach
-       Vorauszahlung der Kursgebühr verbindlich reserviert. Ich habe die Möglichkeit, bis 14 Tage vor
-       Beginn des Kurses von dieser Anmeldung zurück zu treten und die vorausgezahlte Kursgebühr wird
-       zurück erstattet. Mir ist bekannt, dass ich keinen Anspruch auf Rückerstattung der
-       Gebühr bei einem Rücktritt außerhalb dieser Frist habe.
-       Die Kursgebühr Erstattung erfolgt nur mit Attest oder wenn der Platz weitervergeben werden kann. 
+vielen Dank für Ihre Anmeldung für die Veranstaltung "${ result.veranstaltung.Titel }" am ${ format(veranstaltungs_datum, 'dd.MM.yyyy') }. 
+
+Damit der Teilnehmerplatz verbindlich reserviert wird, überweisen Sie bitte bis zum ${ format(date_payment, 'dd.MM.yyyy') } den Betrag in Höhe von ${ result.veranstaltung.Preis } Euro auf folgendes Konto:
+
+IBAN: DE24370502990163007123 
+BIC: COKSDE33
+Bank: Kreissparkasse Köln
+
+Viele Grüße und bis bald!
+ichen    
+
+Rechtliche Hinweise:
+Der Teilnehmerplatz wird für mich nach Vorauszahlung der Kursgebühr verbindlich reserviert. Ich habe die Möglichkeit, bis 14 Tage vor Beginn des Kurses von dieser Anmeldung zurück zu treten und die vorausgezahlte Kursgebühr wird
+zurück erstattet. Mir ist bekannt, dass ich keinen Anspruch auf Rückerstattung der Gebühr bei einem Rücktritt außerhalb dieser Frist habe.
+Die Kursgebühr Erstattung erfolgt nur mit Attest oder wenn der Platz weitervergeben werden kann. 
         `,
        html: `
-        <p>Hallo  ${ result.name },</p>
-        <p>
-        vielen Dank für Ihre Buchung.  </p>
-        <p>Kursname</p>
-        <p>Datum</p>
-        <p>Bitte überweisen Sie den Betrag in Höhe von PREIS innerhalb von XX Tagen auf folgendes Konto:</p>
-        <p>
-        IBAN: DE24370502990163007123 <br />
-        BIC: COKSDE33<br />
-        Bank: Kreissparkasse Köln
-        </p>
-        <p>Der Teilnehmerplatz ist erst mit Zahlungseingang verbindlich reserviert.</p>
-        <p>Viele Grüße und bis bald!<br>
-        ichen    
-        <br />
-        <br />
-        </p>
-        <p>
-        <hr>
-        <br />
-        Rechtliche Hinweise:<br />
-        Der Teilnehmerplatz wird für mich nach
-        Vorauszahlung der Kursgebühr verbindlich reserviert. Ich habe die Möglichkeit, bis 14 Tage vor
-        Beginn des Kurses von dieser Anmeldung zurück zu treten und die vorausgezahlte Kursgebühr wird
-        zurück erstattet. Mir ist bekannt, dass ich keinen Anspruch auf Rückerstattung der
-        Gebühr bei einem Rücktritt außerhalb dieser Frist habe.
-        Die Kursgebühr Erstattung erfolgt nur mit Attest oder wenn der Platz weitervergeben werden kann. 
-        </p>
+<p>Hallo  ${ result.name },</p>
+<p>
+vielen Dank für Ihre Anmeldung für die Veranstaltung "${ result.veranstaltung.Titel }" am ${ format(veranstaltungs_datum, 'dd.MM.yyyy') }.</p>
+<p>
+Damit der Teilnehmerplatz verbindlich reserviert wird, überweisen Sie bitte bis zum ${ format(date_payment, 'dd.MM.yyyy') } den Betrag in Höhe von ${ result.veranstaltung.Preis } Euro auf folgendes Konto:
+</p>
+<p>
+IBAN: DE24370502990163007123 <br />
+BIC: COKSDE33<br />
+Bank: Kreissparkasse Köln
+</p>
+<p>Viele Grüße und bis bald!<br>
+ichen    
+<br />
+<br />
+</p>
+<p>
+<hr>
+<br />
+Rechtliche Hinweise:<br />
+Der Teilnehmerplatz wird für mich nach
+Vorauszahlung der Kursgebühr verbindlich reserviert. Ich habe die Möglichkeit, bis 14 Tage vor
+Beginn des Kurses von dieser Anmeldung zurück zu treten und die vorausgezahlte Kursgebühr wird
+zurück erstattet. Mir ist bekannt, dass ich keinen Anspruch auf Rückerstattung der
+Gebühr bei einem Rücktritt außerhalb dieser Frist habe.
+Die Kursgebühr Erstattung erfolgt nur mit Attest oder wenn der Platz weitervergeben werden kann. 
+</p>
        `,
      });
     }
