27fcc79d98baa8684ee6cfcf57427ef69888ed50
Schaeferjo <mail@joschaefer.com>
2020-08-06 15:23:44 +0200

    Temp e-mail text for succesful booking

--- backend/api/buchung/models/buchung.js
+++ backend/api/buchung/models/buchung.js
@@ -14,9 +14,60 @@ module.exports = {
      await strapi.plugins['email'].services.email.send({
        to: email_adresse,
        from: 'werkhof-ichen@example.org',
-       subject: 'Use strapi email provider successfully',
-       text: 'Hello world!',
-       html: 'Hello world!',
+       subject: 'Ihre Buchung auf werkhof-ichen.de',
+       text: `
+       Hallo  ${ result.name }, 
+       vielen Dank für Ihre Buchung. 
+       Kursname
+       Datum
+       Bitte überweisen Sie den Betrag in Höhe von PREIS innerhalb von XX Tagen auf folgendes Konto:
+       
+       IBAN: DE24370502990163007123 
+       BIC: COKSDE33<br />
+       Bank: Kreissparkasse Köln
+       
+       Der Teilnehmerplatz ist erst mit Zahlungseingang verbindlich reserviert.
+       Viele Grüße und bis bald!
+       ichen    
+
+       Rechtliche Hinweise:
+       Der Teilnehmerplatz wird für mich nach
+       Vorauszahlung der Kursgebühr verbindlich reserviert. Ich habe die Möglichkeit, bis 14 Tage vor
+       Beginn des Kurses von dieser Anmeldung zurück zu treten und die vorausgezahlte Kursgebühr wird
+       zurück erstattet. Mir ist bekannt, dass ich keinen Anspruch auf Rückerstattung der
+       Gebühr bei einem Rücktritt außerhalb dieser Frist habe.
+       Die Kursgebühr Erstattung erfolgt nur mit Attest oder wenn der Platz weitervergeben werden kann. 
+        `,
+       html: `
+        <p>Hallo  ${ result.name },</p>
+        <p>
+        vielen Dank für Ihre Buchung.  </p>
+        <p>Kursname</p>
+        <p>Datum</p>
+        <p>Bitte überweisen Sie den Betrag in Höhe von PREIS innerhalb von XX Tagen auf folgendes Konto:</p>
+        <p>
+        IBAN: DE24370502990163007123 <br />
+        BIC: COKSDE33<br />
+        Bank: Kreissparkasse Köln
+        </p>
+        <p>Der Teilnehmerplatz ist erst mit Zahlungseingang verbindlich reserviert.</p>
+        <p>Viele Grüße und bis bald!<br>
+        ichen    
+        <br />
+        <br />
+        </p>
+        <p>
+        <hr>
+        <br />
+        Rechtliche Hinweise:<br />
+        Der Teilnehmerplatz wird für mich nach
+        Vorauszahlung der Kursgebühr verbindlich reserviert. Ich habe die Möglichkeit, bis 14 Tage vor
+        Beginn des Kurses von dieser Anmeldung zurück zu treten und die vorausgezahlte Kursgebühr wird
+        zurück erstattet. Mir ist bekannt, dass ich keinen Anspruch auf Rückerstattung der
+        Gebühr bei einem Rücktritt außerhalb dieser Frist habe.
+        Die Kursgebühr Erstattung erfolgt nur mit Attest oder wenn der Platz weitervergeben werden kann. 
+        </p>
+       `,
      });
     }
   }
