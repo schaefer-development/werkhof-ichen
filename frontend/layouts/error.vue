10f1700e7c927fb9e3039d87002b79b88f9e0804
Schaeferjo <mail@joschaefer.com>
2020-08-11 14:46:45 +0200

    Error page styling

--- frontend/layouts/error.vue
+++ frontend/layouts/error.vue
@@ -1,15 +1,39 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
-  </v-app>
+  <v-container>
+    <v-row class="pb-xs-6">
+      <v-col cols="12">
+        <v-img
+          src="/header_images/header_ghosts.jpg"
+          alt="werkhof ichen: Andere Textilien"
+          class="mb-5 mr-3 ml-3"
+        />
+      </v-col>
+      <v-col
+        cols="12"
+        xs="12"
+        sm="8"
+        offset-sm="4"
+        md="8"
+        offset-md="4"
+        lg="8"
+        offset-lg="4"
+        xl="8"
+        offset-xl="4"
+      >
+        <v-card rounded="0" flat class="ichen_beige">
+          <v-card-title v-if="error.statusCode === 404">{{ pageNotFound }}</v-card-title>
+          <v-card-title v-else>{{ otherError }}</v-card-title>
+          <v-card-text>
+            <p>Ups ... Da ist was schiefgelaufen.</p>
+            <p>
+              Möchten Sie zurück zur
+              <NuxtLink to="/">Startseite</NuxtLink>?
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -23,8 +47,8 @@ export default {
   },
   data() {
     return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred',
+      pageNotFound: 'Fehler 404 – Seite nicht gefunden',
+      otherError: 'Ein Fehler ist aufgetreten',
     }
   },
   head() {
@@ -37,8 +61,3 @@ export default {
 }
 </script>
 
-<style scoped>
-h1 {
-  font-size: 20px;
-}
-</style>
