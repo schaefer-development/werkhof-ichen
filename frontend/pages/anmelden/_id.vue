20d84e4246df66bc07e8594ba31e09c92e6a0655
roschaefer <git@roschaefer.de>
2020-08-12 18:13:37 +0200

    refactor: clean up unnecessary markdown

--- frontend/pages/anmelden/_id.vue
+++ frontend/pages/anmelden/_id.vue
@@ -1,5 +1,18 @@
 <template>
-  <booking-form :veranstaltung="veranstaltung"></booking-form>
+  <v-container>
+    <v-row>
+      <v-col cols="12" xs="12" sm="4" md="4" lg="4" xl="4">
+        <v-img
+          class="white--text align-end"
+          height="200px"
+          :src="veranstaltung.Vorschaubild.url"
+        ></v-img>
+      </v-col>
+      <v-col cols="12" xs="12" sm="8" md="8" lg="8" xl="8">
+        <booking-form :veranstaltung="veranstaltung"></booking-form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
