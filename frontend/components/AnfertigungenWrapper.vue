610d54f2f3d299eac437fe7e56469ac2477ea67f
Schaeferjo <mail@joschaefer.com>
2020-08-19 21:40:54 +0200

    Refactored submenu

--- frontend/components/AnfertigungenWrapper.vue
+++ frontend/components/AnfertigungenWrapper.vue
@@ -2,30 +2,22 @@
   <v-container>
     <v-row class="pb-xs-6">
       <v-col cols="12">
-        <slot name="banner"> </slot>
+        <slot name="banner"></slot>
       </v-col>
     </v-row>
-    <v-row align="baseline">
-      <v-col
-        cols="12"
-        xs="12"
-        sm="12"
-        md="4"
-        lg="4"
-        xl="4"
-        class="subnavi_left"
-      >
+    <v-row>
+      <v-col cols="12" xs="12" sm="12" md="4" lg="4" xl="4" class="subnavi_left">
         <v-card rounded="0" flat class="ichen_green pa-3">
           <v-card-title class="white--text">Anfertigungen</v-card-title>
-          <v-divider class="white"></v-divider>
+          <div class="submenu_divider"></div>
           <v-card-text>
-            <v-list class="white--text" nav color="transparent">
-              <v-list-item nuxt link to="/anfertigungen/leder"
-                ><strong>Leder</strong></v-list-item
-              >
-              <v-list-item nuxt link to="/anfertigungen/weitere_textilien"
-                ><strong>Weitere Textilien</strong></v-list-item
-              >
+            <v-list class="white--text" flat nav color="transparent">
+              <v-list-item nuxt link to="/anfertigungen/leder">
+                <strong>Leder</strong>
+              </v-list-item>
+              <v-list-item nuxt link to="/anfertigungen/weitere_textilien">
+                <strong>Weitere Textilien</strong>
+              </v-list-item>
             </v-list>
           </v-card-text>
         </v-card>
@@ -36,3 +28,9 @@
     </v-row>
   </v-container>
 </template>
+
+<style scoped>
+.submenu_divider {
+  border-bottom: 1px dashed #fff;
+}
+</style>
