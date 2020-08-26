55102964223374fdb66c9558793892fe99e11882
Schaeferjo <mail@joschaefer.com>
2020-08-26 14:05:49 +0200

    Changed booking date ahead from 14 days to 3 days

--- backend/emailTemplates/anmeldung.js
+++ backend/emailTemplates/anmeldung.js
@@ -1,17 +1,17 @@
 const { addDays } = require('date-fns')
 const { format } = require('date-fns')
 
-module.exports = function(anmeldung) {
-  const veranstaltungs_datum = Date.parse (anmeldung.veranstaltung.Datum)
-  const date_ahead = addDays(veranstaltungs_datum, 14)
-  const date_payment = new Date(Math.min ( veranstaltungs_datum, date_ahead ))
+module.exports = function (anmeldung) {
+  const veranstaltungs_datum = Date.parse(anmeldung.veranstaltung.Datum)
+  const date_ahead = addDays(veranstaltungs_datum, 3)
+  const date_payment = new Date(Math.min(veranstaltungs_datum, date_ahead))
   return {
     text: `
-Hallo  ${ anmeldung.name },
+Hallo  ${ anmeldung.name},
 
-vielen Dank für Ihre Anmeldung für die Veranstaltung "${ anmeldung.veranstaltung.Titel }" am ${ format(veranstaltungs_datum, 'dd.MM.yyyy') }.
+vielen Dank für Ihre Anmeldung für die Veranstaltung "${ anmeldung.veranstaltung.Titel}" am ${format(veranstaltungs_datum, 'dd.MM.yyyy')}.
 
-Damit der Teilnehmerplatz verbindlich reserviert wird, überweisen Sie bitte bis zum ${ format(date_payment, 'dd.MM.yyyy') } den Betrag in Höhe von ${ anmeldung.veranstaltung.Preis } Euro auf folgendes Konto:
+Damit der Teilnehmerplatz verbindlich reserviert wird, überweisen Sie bitte bis zum ${ format(date_payment, 'dd.MM.yyyy')} den Betrag in Höhe von ${anmeldung.veranstaltung.Preis} Euro auf folgendes Konto:
 
 IBAN: DE24370502990163007123
 BIC: COKSDE33
@@ -26,11 +26,11 @@ zurück erstattet. Mir ist bekannt, dass ich keinen Anspruch auf Rückerstattung
 Die Kursgebühr Erstattung erfolgt nur mit Attest oder wenn der Platz weitervergeben werden kann.
 `,
     html: `
-<p>Hallo  ${ anmeldung.name },</p>
+<p>Hallo  ${ anmeldung.name},</p>
 <p>
-vielen Dank für Ihre Anmeldung für die Veranstaltung "${ anmeldung.veranstaltung.Titel }" am ${ format(veranstaltungs_datum, 'dd.MM.yyyy') }.</p>
+vielen Dank für Ihre Anmeldung für die Veranstaltung "${ anmeldung.veranstaltung.Titel}" am ${format(veranstaltungs_datum, 'dd.MM.yyyy')}.</p>
 <p>
-Damit der Teilnehmerplatz verbindlich reserviert wird, überweisen Sie bitte bis zum ${ format(date_payment, 'dd.MM.yyyy') } den Betrag in Höhe von ${ anmeldung.veranstaltung.Preis } Euro auf folgendes Konto:
+Damit der Teilnehmerplatz verbindlich reserviert wird, überweisen Sie bitte bis zum ${ format(date_payment, 'dd.MM.yyyy')} den Betrag in Höhe von ${anmeldung.veranstaltung.Preis} Euro auf folgendes Konto:
 </p>
 <p>
 IBAN: DE24370502990163007123 <br />
