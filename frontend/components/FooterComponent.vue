54071dfebfca98d43d6c271f29e0e178f8f902d1
Schaeferjo <mail@joschaefer.com>
2020-08-27 19:36:05 +0200

    Styling

--- frontend/components/FooterComponent.vue
+++ frontend/components/FooterComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer color="ichen_brown" dark padless>
+  <v-footer dark padless>
     <v-container fluid class="pt-0 pb-3">
       <v-row class="d-none d-md-block">
         <div id="footer-component-footer-gallery">
@@ -9,10 +9,7 @@
           <v-img src="/footer_images/hearts.jpg" alt="Herzen"></v-img>
           <v-img src="/footer_images/sew.jpg" alt="Nähen"></v-img>
           <v-img src="/footer_images/collection2.jpg" alt="Sammlung"></v-img>
-          <v-img
-            src="/footer_images/kater_schroeder.jpg"
-            alt="Kater Schröder"
-          ></v-img>
+          <v-img src="/footer_images/kater_schroeder.jpg" alt="Kater Schröder"></v-img>
           <v-img src="/footer_images/flower.jpg" alt="Blumen"></v-img>
           <v-img src="/footer_images/texture.jpg" alt="Textur"></v-img>
           <v-img src="/footer_images/lamp.jpg" alt="Lampe"></v-img>
@@ -22,30 +19,28 @@
       <v-row
         justify="center"
         align="center"
-        class="footer-component-bottom-row pt-8 pl-12 pb-3 pr-12"
+        class="footer-component-bottom-row pt-4 pl-12 pb-1 pr-12"
       >
-        <v-col cols="12" sm="12" md="12" lg="2" xl="2" align="center">
-          <v-img
-            src="/logo_ichen_outline.svg"
-            alt="Logo Werkhof ichen"
-            max-width="90px"
-          />
-        </v-col>
-        <v-col cols="12" sm="12" md="4" lg="3" xl="2">
-          Telefon:
-          <a href="tel:+4922463197">02246 . 31 97</a>
-          <br />E-Mail:
-          <a href="mailto:info@werkhof-ichen.de">info@werkhof-ichen.de</a>
+        <v-col cols="12" sm="6" md="4" lg="2" xl="2" align="center">
+          <v-img src="/logo_ichen_outline.svg" alt="Logo Werkhof ichen" max-width="120px" />
         </v-col>
         <v-col cols="12" sm="6" md="4" lg="2" xl="2">
-          <nuxt-link to="/datenschutz">Datenschutz</nuxt-link>
-          <br />
-          <nuxt-link to="/impressum">Impressum</nuxt-link>
-        </v-col>
-        <v-col cols="12" sm="6" md="4" lg="2" xl="2">
-          <nuxt-link to="/agb">AGB</nuxt-link>
-          <br />
-          Copyright {{ new Date().getFullYear() }}
+          <p>
+            <a href="tel:+4922463197">02246 . 31 97</a>
+            <br />
+            <a href="mailto:info@werkhof-ichen.de">info@werkhof-ichen.de</a>
+          </p>
+
+          <p>
+            <nuxt-link to="/datenschutz">Datenschutz</nuxt-link>
+            <br />
+            <nuxt-link to="/impressum">Impressum</nuxt-link>
+          </p>
+          <p>
+            <nuxt-link to="/agb">AGB</nuxt-link>
+            <br />
+            Copyright {{ new Date().getFullYear() }}
+          </p>
         </v-col>
       </v-row>
     </v-container>
@@ -53,6 +48,9 @@
 </template>
 
 <style scoped lang="scss">
+footer {
+  background-color: rgba(54, 47, 43, 0.93) !important;
+}
 #footer-component-footer-gallery {
   width: 100%;
   height: 10vw;
@@ -62,7 +60,7 @@
   }
 }
 .footer-component-bottom-row {
-  font-size: 0.9em;
+  font-size: 0.8em;
   a {
     color: #fff;
     text-decoration: none;
--- frontend/pages/veranstaltung/_id.vue
+++ frontend/pages/veranstaltung/_id.vue
@@ -2,19 +2,13 @@
   <v-container>
     <v-row>
       <v-col cols="12" xs="12" sm="4" md="4" lg="4" xl="4">
-        <v-img
-          class="white--text align-end"
-          height="200px"
-          :src="veranstaltung.Vorschaubild.url"
-        ></v-img>
+        <v-img class="white--text align-end" height="200px" :src="veranstaltung.Vorschaubild.url"></v-img>
       </v-col>
       <v-col cols="12" xs="12" sm="8" md="8" lg="8" xl="8">
         <v-card rounded="0" flat class="px-6 pt-8">
           <v-list-item>
             <v-list-item-content>
-              <v-list-item-title class="ichen_blue--text text-h2">
-                {{ veranstaltung.Titel }}
-              </v-list-item-title>
+              <v-list-item-title class="ichen_blue--text text-h2">{{ veranstaltung.Titel }}</v-list-item-title>
             </v-list-item-content>
             <v-list-item-action>
               <v-btn
@@ -31,7 +25,7 @@
             </v-list-item-action>
           </v-list-item>
           <v-card-text>
-            <strong> {{ veranstaltung.Datum | formatDate }} Uhr </strong>
+            <strong>{{ veranstaltung.Datum | formatDate }} Uhr</strong>
           </v-card-text>
           <v-card-text>
             <registration-form
@@ -39,8 +33,7 @@
               :veranstaltung="veranstaltung"
               @cancel="toggleRegistration"
               @confirm="cancel"
-            >
-            </registration-form>
+            ></registration-form>
             <template v-else>
               <p v-html="$md.render(veranstaltung.Beschreibung)"></p>
               <p>
@@ -51,27 +44,21 @@
                 text
                 type="success"
                 class="font-weight-bold"
-                >Es sind noch Plätze frei!</v-alert
-              >
+              >Es sind noch Plätze frei!</v-alert>
               <v-alert
                 v-else
                 text
+                flat
                 type="info"
                 icon="mdi-alert-circle"
                 class="font-weight-bold"
-                >Es gibt keine freien Plätze mehr. Sie können sich aber auf die
-                Warteliste setzen.</v-alert
-              >
+              >Aktuell sind alle Plätze belegt. Aber für den Fall, dass doch noch ein Platz frei wird, können Sie sich hier auf die Warteliste setzen.</v-alert>
               <v-btn
                 depressed
                 color="ichen_red white--text"
                 @click="toggleRegistration"
-              >
-                {{ available ? 'Zur Anmeldung' : 'Zur Warteliste' }}
-              </v-btn>
-              <v-btn depressed color="ichen_blue white--text" @click="cancel">
-                Zurück
-              </v-btn>
+              >{{ available ? 'Zur Anmeldung' : 'Zur Warteliste' }}</v-btn>
+              <v-btn depressed color="ichen_blue white--text" @click="cancel">Zurück</v-btn>
             </template>
           </v-card-text>
         </v-card>
