5fc2f4587b3fd04279a8946b7dc92463924c118e
Schaeferjo <mail@joschaefer.com>
2020-09-08 14:06:26 +0200

    Social media icons next to hamburger icon

--- frontend/components/HeaderComponent.vue
+++ frontend/components/HeaderComponent.vue
@@ -6,33 +6,28 @@
           <v-img src="/logo_ichen.svg" alt="Logo Werkhof ichen" max-width="150px" />
         </nuxt-link>
         <v-spacer></v-spacer>
-
-        <v-menu left bottom>
-          <v-list-item>
-            <v-btn
-              icon
-              link
-              target="_blank"
-              rel="noopener"
-              color="ichen_blue"
-              aria-label="Facebook"
-              href="https://de-de.facebook.com/pages/category/Education/Werkhof-ichen-130771390426775/"
-            >
-              <v-icon medium>mdi-facebook</v-icon>
-            </v-btn>
-            <v-btn
-              link
-              target="_blank"
-              rel="noopener"
-              color="ichen_blue"
-              aria-label="Instagram"
-              href="https://www.instagram.com/irmina_schmitz/"
-              icon
-            >
-              <v-icon medium>mdi-instagram</v-icon>
-            </v-btn>
-          </v-list-item>
-        </v-menu>
+        <v-btn
+          icon
+          link
+          target="_blank"
+          rel="noopener"
+          color="ichen_blue"
+          aria-label="Facebook"
+          href="https://de-de.facebook.com/pages/category/Education/Werkhof-ichen-130771390426775/"
+        >
+          <v-icon medium>mdi-facebook</v-icon>
+        </v-btn>
+        <v-btn
+          link
+          target="_blank"
+          rel="noopener"
+          color="ichen_blue"
+          aria-label="Instagram"
+          href="https://www.instagram.com/irmina_schmitz/"
+          icon
+        >
+          <v-icon medium>mdi-instagram</v-icon>
+        </v-btn>
         <v-app-bar-nav-icon aria-label="Open Drawer" @click.stop="toggleDrawer">
           <v-icon size="50">mdi-menu</v-icon>
         </v-app-bar-nav-icon>
