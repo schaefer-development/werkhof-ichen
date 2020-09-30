c3edd06934b9c15959441e7a19df9dbd06947c8e
Schaeferjo <mail@joschaefer.com>
2020-09-30 15:20:02 +0200

    Text correction: weitere textilien

--- frontend/pages/anfertigungen/weitere_materialien.vue
+++ frontend/pages/anfertigungen/weitere_materialien.vue
@@ -1,14 +1,11 @@
 <template>
   <anfertigungen-wrapper>
     <template v-slot:banner>
-      <v-img
-        src="/header_images/header_other_materials.jpg"
-        alt="werkhof ichen: Andere Textilien"
-      />
+      <v-img src="/header_images/header_other_materials.jpg" alt="werkhof ichen: Andere Textilien" />
     </template>
     <v-card rounded="0" flat>
       <v-card-title class="text-h2">Weitere Textilien</v-card-title>
-      <v-card-text></v-card-text>
+      <v-card-text>Naturstoffe, wie Walkware aus reiner Wolle, Baumwolle, Leinen. Auch gewachster Canvas für Taschen usw., gewachste Baumwolle für Jacken, Baumwollplüsch zum füttern von Jacken oder Decken, aber auch Mikrofaser, Softshell, gummierte Gewebe, Lkw-Plane, alte Fahrradschläuche/Feuerwehrschläuche finden Verwendung in meiner Werkstatt ( ich experimentiere gerne).</v-card-text>
     </v-card>
   </anfertigungen-wrapper>
 </template>
