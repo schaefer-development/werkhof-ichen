861a0928591383b2fad800b0cf6d498e62c54492
roschaefer <git@roschaefer.de>
2020-10-05 13:47:25 +0200

    fix: Only log write requests to /anmeldungs
    We don't want to write our admin passwords to the logs!

--- backend/middlewares/requestsLogger/index.js
+++ backend/middlewares/requestsLogger/index.js
@@ -4,7 +4,7 @@ module.exports = strapi => {
       strapi.app.use(async (ctx, next) => {
         await next();
         const { url, method, body } = ctx.request
-        if (['POST', 'PUT', 'PATCH', 'DELETE'].includes(method)) {
+        if(url === /anmeldungs/ && ['POST', 'PUT', 'PATCH', 'DELETE'].includes(method)) {
           strapi.log.debug(method, url, body)
         }
       });
