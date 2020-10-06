785cc089d422de98a4b95edfde59fe2b633a14ee
Schaeferjo <mail@joschaefer.com>
2020-10-06 16:45:52 +0200

    Change h2 size

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -37,8 +37,8 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: 'Werkhof ichen %s',
+    title: '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
