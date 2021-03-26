06163003af6a3deb88b0bb65ee48a7775ef82c47
Schaeferjo <mail@joschaefer.com>
2021-03-26 16:19:38 +0100

    fix: ignore unkown heroku certificates
    https://nodejs.org/api/tls.html#tls_tls_createserver_options_secureconnectionlistener
    https://node-postgres.com/features/ssl
    https://github.com/brianc/node-postgres/issues/2009
    https://github.com/strapi/strapi/issues/5696

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -10,6 +10,9 @@ module.exports = ({ env }) => ({
         database: env('DATABASE_NAME', 'strapi'),
         username: env('DATABASE_USERNAME', ''),
         password: env('DATABASE_PASSWORD', ''),
+        ssl: {
+          rejectUnauthorized: false, // For self-signed certificates
+        },
       },
       options: {
         ssl: true,
